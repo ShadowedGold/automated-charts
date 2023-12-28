7d01b141f77513cb71d4ef375e92158138fa1f97
ShadowedGold <77082877+ShadowedGold@users.noreply.github.com>
2023-12-28 19:54:34 +0800

    chaos factor

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var chaosFactor = 9;
+var chaosFactor = 5;
 
 function pickFromArray(table) {
   let dSize = table[table.length - 1].chance;
@@ -44,6 +44,13 @@ function pickFromFateChart(odds) {
   }
 }
 
+function createLabel(name, parentEl) {
+  let label = document.createElement('div');
+  label.classList.add('label');
+  label.innerText = name;
+  parentEl.appendChild(label);
+}
+
 function createButton(table, parentEl, section) {
   let button = document.createElement('div');
   button.classList.add("button");
@@ -80,10 +87,7 @@ function createSection(name, array) {
   let section = document.createElement('div');
   section.classList.add('section');
 
-  let label = document.createElement('div');
-  label.classList.add('label');
-  label.innerText = name;
-  section.appendChild(label);
+  createLabel(name, section);
 
   let buttonHolder = document.createElement('div');
   buttonHolder.classList.add('buttonHolder');
@@ -97,11 +101,55 @@ function createSection(name, array) {
   document.body.appendChild(section);
 }
 
-/* --- Setting Up Elements ------------------------------- */
+/* --- Chaos Factor ------------------------------------- */
+
+let chaosFactorSection = document.createElement('div');
+chaosFactorSection.classList.add("section", "chaosFactor");
+
+createLabel("Chaos Factor", chaosFactorSection);
+
+let chaosFactorHolder = document.createElement('div');
+chaosFactorHolder.classList.add("chaosFactorHolder");
+
+let chaosFactorDownBtn = document.createElement('div');
+chaosFactorDownBtn.classList.add("button");
+chaosFactorDownBtn.innerText = "-";
+chaosFactorDownBtn.onclick = () => {
+  if (chaosFactor > 1) {
+    chaosFactor--;
+    chaosFactorEl.innerText = chaosFactor;
+  }
+};
+chaosFactorHolder.appendChild(chaosFactorDownBtn);
+
+let chaosFactorEl = document.createElement('div');
+chaosFactorEl.classList.add("tracked");
+chaosFactorEl.innerText = chaosFactor;
+chaosFactorHolder.appendChild(chaosFactorEl);
+
+let chaosFactorUpBtn = document.createElement('div');
+chaosFactorUpBtn.classList.add("button");
+chaosFactorUpBtn.innerText = "+";
+chaosFactorUpBtn.onclick = () => {
+  if (chaosFactor < 9) {
+    chaosFactor++;
+    chaosFactorEl.innerText = chaosFactor;
+  }
+};
+chaosFactorHolder.appendChild(chaosFactorUpBtn);
+
+chaosFactorSection.appendChild(chaosFactorHolder);
+
+document.body.appendChild(chaosFactorSection);
+
+/* --- Results ------------------------------------------ */
 
 let results = document.createElement('div');
 results.classList.add("section", "results");
+
 document.body.appendChild(results);
+
+/* --- Buttons ------------------------------------------ */
 
 createSection("fate chart", odds);
 
